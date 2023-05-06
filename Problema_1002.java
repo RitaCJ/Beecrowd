491ec681ef2a74604d4d2a338cdcf298c38f492f
unknown <Yuu-rita>
2023-05-06 18:42:17 +0100

    Terceiro Problema

---
+++ Problema_1002.java
@@ -0,0 +1,20 @@
+package Problemas_Resolvidos.Beecrowd;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Problema_1002 {
+    public static void main (String []args){
+
+        double R, Result;
+        Scanner snr = new Scanner(System.in);
+    try{
+        R = snr.nextDouble();
+        Result = (3.14159 * Math.pow(R, 2));
+       String A = String.format("%.4f", Result);
+        System.out.println("A=" + A);
+    }finally{
+        snr.close();
+    }
+        
+    }
+}
