d08b3d839113290bd2fd57526ab611a2887a751a
unknown <Yuu-rita>
2023-06-09 21:13:52 +0100

    Resolvido

---
+++ Problema_1014.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Problema_1014 {
+    public static void main(String args[]){
+
+        int x;
+        float y;
+        double consumption;
+
+        Scanner input = new Scanner(System.in);
+
+        x = input.nextInt();
+        y = input.nextFloat();
+
+        consumption = x / y;
+
+        String c = String.format("%.3f", consumption);
+
+        System.out.println(c + " km/l");
+
+    }
+}
